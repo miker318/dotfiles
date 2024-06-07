9885f3e63a4659043c72e82c93df38b8b672f9d5
miker318 <66387595+miker318@users.noreply.github.com>
2024-06-07 15:12:37 -0400

    Create mrose.nix

---
+++ .config/nixos/users/mrose.nix
@@ -0,0 +1,78 @@
+# home/mrose.nix
+# Requires home-manager flake
+{ pkgs, ... }:
+let
+  elem-wp-path = "${pkgs.pantheon.elementary-wallpapers}/share/backgrounds";
+in {
+
+  home.username = "mrose";
+  home.homeDirectory = "/home/mrose";
+
+  home.packages = with pkgs; [
+    doctl
+    flatpak-builder
+    mkcert
+  ];
+
+  services.syncthing.enable = true;
+  services.pueue = {
+    enable = true;
+    settings = {
+      client = {
+        dark_mode = false;
+        show_expanded_aliases = false;
+      };
+      daemon = {
+        default_parallel_tasks = 2;
+        pause_group_on_failure = false;
+        pause_all_on_failure = false;
+      };
+      shared = {
+        use_unix_socket = true;
+      };
+    };
+  };
+
+  helix.lsps = [
+    "bash"
+    "css"
+    "dockerfile"
+    "html"
+    "java"
+    "json"
+    "markdown"
+    "nix"
+    "rust"
+    "toml"
+    "typescript"
+    "vala"
+    "yaml"
+  ];
+
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      picture-uri = "${elem-wp-path}/odin.jpg";
+      picture-uri-dark = "${elem-wp-path}/odin-dark.jpg";
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "gsconnect@andyholmes.github.io"
+        "tailscale@joaophi.github.com"
+        "nightthemeswitcher@romainvigier.fr"
+        "GPaste@gnome-shell-extensions.gnome.org"
+        "blur-my-shell@aunetx"
+        "luminus-shell-y@dikasp.gitlab"
+      ];
+      favorite-apps = [
+        "firefox.desktop"
+        "code.desktop"
+        "beeper.desktop"
+        "org.gnome.Nautilus.desktop"
+        "com.raggesilver.BlackBox.desktop"
+      ];
+    };
+  };
+
+  home.stateVersion = "24.11";
+}
