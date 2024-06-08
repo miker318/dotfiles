593fac518783c7ed5a5f15df3b1d2ca012707309
miker318 <66387595+miker318@users.noreply.github.com>
2024-06-07 21:34:05 -0400

    personalize git.nix

--- .config/nixos/home/git.nix
+++ .config/nixos/home/git.nix
@@ -3,10 +3,10 @@
 
   programs.git.enable = lib.mkDefault true;
   programs.git.package = lib.mkDefault pkgs.gitAndTools.gitFull;
-  programs.git.userName = lib.mkDefault "Kevin J Hoerr";
-  programs.git.userEmail = lib.mkDefault "kjhoerr@protonmail.com";
-  programs.git.signing.key = lib.mkDefault "BEDBA29269ED7111";
-  programs.git.signing.signByDefault = lib.mkDefault true;
+  programs.git.userName = lib.mkDefault "Michael Rose";
+  programs.git.userEmail = lib.mkDefault "mike@erose.org";
+ # programs.git.signing.key = lib.mkDefault "BEDBA29269ED7111";
+ # programs.git.signing.signByDefault = lib.mkDefault true;
   programs.git.extraConfig.init.defaultBranch = "trunk";
   programs.git.extraConfig.core.editor = "hx";
   programs.git.extraConfig.color.ui = "always";
