06313c4840efb4418b7a74c5ec23b7eab9625e96
miker318 <66387595+miker318@users.noreply.github.com>
2024-06-07 21:32:34 -0400

    personalize profiles-rebuild.sh

--- .config/nixos/scripts/profiles-rebuild.sh
+++ .config/nixos/scripts/profiles-rebuild.sh
@@ -5,7 +5,7 @@
 ## provided as option(s) to nix build
 
 ## Setup - strongly assume defaults
-SOURCE="github:kjhoerr/dotfiles"
+SOURCE="github:miker318/dotfiles?ref=testing"
 SYSPROFILE="/nix/var/nix/profiles/system"
 HOSTNAME=$(hostname)
 USERNAME=$(whoami)
